b998359ba60fb811ac91fc33249e478cd5fa4d66
UzairAlii <uzairali89347@gmail.com>
2025-10-20 01:08:56 +0500

    routing error fixed

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 import { LanguageProvider } from './context/LanguageContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <Router>
     <LanguageProvider>
     <App />
     </LanguageProvider>
-  </BrowserRouter>
+  </Router>
   </StrictMode>
 )
